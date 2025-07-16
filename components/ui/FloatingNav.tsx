6c358f28f86c06eab70282cc5475885b953440c8
Umar <uj2849@gmail.com>
2025-07-16 18:49:05 +0500

    navbar changes

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -71,7 +71,7 @@ export const FloatingNav = ({
           exit={{ y: -100, opacity: 0 }}
           transition={{ duration: 0.2 }}
           className={cn(
-            "flex max-w-fit fixed top-16 md:top-10 inset-x-0 mx-auto border border-white/[0.2] bg-black-100 rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-5 md:px-10 py-5 items-center justify-center space-x-4",
+            "flex max-w-fit fixed top-10  inset-x-0 mx-auto border border-white/[0.2] bg-black-100 rounded-full shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-5 md:px-10 py-5 items-center justify-center space-x-4",
             className
           )}
         >
